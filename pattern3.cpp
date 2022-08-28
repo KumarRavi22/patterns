2344699e1bf0a95a9017c52097c1d68b772dd4db
Ravi Kumar <107766052+KumarRavi22@users.noreply.github.com>
2022-08-28 23:27:24 +0530

    Add files via upload

---
+++ pattern3.cpp
@@ -0,0 +1,26 @@
+/*
+1234
+1234
+1234
+1234
+
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+  int n,d;
+  cin>>n;
+  int i=1;
+  while(i<=n)
+  {
+    int j=1;
+    while(j<=n)
+    {
+      cout<<j; 
+    }
+    cout<<endl;
+    i=i+1;
+  }
+}
+
