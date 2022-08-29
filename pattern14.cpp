e050f59115faf03646d9389099ea6513ae0dfd9d
Ravi Kumar <107766052+KumarRavi22@users.noreply.github.com>
2022-08-29 23:12:52 +0530

    Add files via upload

---
+++ pattern14.cpp
@@ -0,0 +1,28 @@
+/*
+    A B C D E 
+    A B C D E
+    A B C D E
+    A B C D E
+    A B C D E
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int i = 1;
+    while (i <= n)
+    {
+        int j = 1;
+        while (j <= n)
+        {
+            char d = j - 1 + 'A';
+            cout << d << " ";
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+}
